5b715335de83d35de5d9c4e29f7b31478387f562
yogesh <yogisharma1062@gmail.com>
2024-04-11 13:34:19 +0530

    Addon to Debouncing

--- concepts/debouncing.js
+++ concepts/debouncing.js
@@ -1,3 +1,11 @@
+/// DEBOUNCING in JS
+//Defination: Debouncing in JS is a technique used to limit the rate at which function is called, typically used in scenarios where a function could triggered rapidly(eg., due to frequent user input events like scrolling or typing). Debouncing ensures that the function is only executed after a certain amount of time has passed since the last call to the function.
+
+//Why?
+// 1. Performance
+// 2. User Experience
+
+
 //Tradtional Way
 let counter = 0;
 function getInfo(){
